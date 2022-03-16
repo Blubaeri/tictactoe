e40c1fa07f9d8e46bb53aefbeb60891e32e75b51
Blubaeri <evanlin2912@gmail.com>
2022-03-16 11:47:19 -0400

    printBoard finished?

--- tictactoe.py
+++ tictactoe.py
@@ -13,21 +13,44 @@ def printTitle():
         '''
     print(str)
 
-# Expected input range: 0 to 8
+# Expected input range: 1-9
 # Output: Checks if a tile is avaliable
-def isPositionAvaliable(row, col):
+def isPositionAvaliable(tile):
     str = ''
 
-# Expected input range: TBD
+# Expected input range: 1-9, choice of X or O
 # Output: Prints the board with updated choices
-def printBoard():
-    str = ''''''
+def printBoard(tile, choice):
+    str = '''
+    1 | 2 | 3
+    4 | 5 | 6
+    7 | 8 | 9
+    '''
+    arr = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8 , 9]]
+    for x in arr:
+        if tile in x:
+            arr[arr.index(x)][x.index(tile)] = choice
 
+    for x in arr:
+        for y in x:
+            print(y, end = "|")
+        print('\n')
+
+# Expected input range: TBD
+# Output: Checks if the game is over
+def victoryCheck():
+    str = ''
+    
+# Expected input range: TBD
+# Output: Runs the other functions to play the game
 def playGame():
     str = ''
 
 def main():
     printTitle()
+
 
 if(__name__ == "__main__"):
     main()
