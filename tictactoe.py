118438fd8edc878fb382a37b19f82a43ebebf5ad
Blubaeri <evanlin2912@gmail.com>
2022-03-15 23:50:07 -0400

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,33 @@
+
+# Expected input range: None
+# Output: Prints the title and rules of the game
+def printTitle():
+    str = '''
+        ----------------     1 | 2 | 3
+        TIC - TAC - TOE      4 | 5 | 6
+        ----------------     7 | 8 | 9
+
+        TO PLAY TIC - TAC - TOE, YOU NEED TO GET THREE IN A ROW.
+        YOUR CHOICES ARE BETWEEN 1 TO 9.
+
+        '''
+    print(str)
+
+# Expected input range: 0 to 8
+# Output: Checks if a tile is avaliable
+def isPositionAvaliable(row, col):
+    str = ''
+
+# Expected input range: TBD
+# Output: Prints the board with updated choices
+def printBoard():
+    str = ''''''
+
+def playGame():
+    str = ''
+
+def main():
+    printTitle()
+
+if(__name__ == "__main__"):
+    main()
